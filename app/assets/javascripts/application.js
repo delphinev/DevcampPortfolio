0f77edef4d5925aebda291d2678e255b477007e6
Delphine Vilaysack <dvilaysack@trustpair.fr>
2018-12-29 11:28:11 +0100

    Integrate initial drag and drop implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require popper
 //= require activestorage
 //= require bootstrap-sprockets
 //= require jquery-ui
